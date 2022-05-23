79a7521c1aee83a38481e5b3ee8e9e360c8fdfac
seung-woo-ryu <tmddn645@naver.com>
2022-05-23 19:22:05 +0900

    add: members.jsp

--- servlet/src/main/java/hello/servlet/ServletApplication.java
+++ servlet/src/main/java/hello/servlet/ServletApplication.java
@@ -2,9 +2,7 @@ package hello.servlet;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.web.servlet.ServletComponentScan;
 
-@ServletComponentScan
 @SpringBootApplication
 public class ServletApplication {
 
